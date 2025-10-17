091be8338fb927136fb10fcfeb4f56e87a73ae1c
Joshua Sarcol <23100968@usc.edu.ph>
2025-10-17 22:59:49 +0800

    consistent spacing 2

--- Unit 3/client.py
+++ Unit 3/client.py
@@ -17,9 +17,9 @@ def receiveMsg(conn):
             decrypted_msg = client_private_key.decrypt(
                 data,
                 padding.OAEP(
-                    mgf=padding.MGF1(algorithm=hashes.SHA256()),
-                    algorithm=hashes.SHA256(),
-                    label=None
+                    mgf = padding.MGF1(algorithm = hashes.SHA256()),
+                    algorithm = hashes.SHA256(),
+                    label = None
                 )
             )
             print('\nMessage Received: {}'.format(decrypted_msg.decode()))
